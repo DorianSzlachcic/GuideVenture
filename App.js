0d9e3d808cf358eece01f7523f59b44701c58e96
PawelW02 <paawlicho1@gmail.com>
2024-04-11 01:52:05 +0200

    pierwsza wersja quizu

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { RadioButton } from './components/RadioButton';
+import Quiz from './quiz';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <RadioButton answer="ABC"/>
-      <RadioButton answer="ABC"/>
-    </View>
+    // <View style={styles.container}>
+    //   <StatusBar style="auto" />
+    //   <RadioButton answer="ABC"/>
+    //   <RadioButton answer="ABC"/>
+    // </View>
+    Quiz
   );
 }
 
